12eb55109b9355dbbf5807da4d9aebe650b9d8a8
Nika <slesareva-n-r@ya.ru>
2025-11-05 16:40:34 +0300

    task: countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+timer = document.getElementById("timer");
+let message = "Вы победили в конкурсе!";
+
+let timerset = setInterval(() => {
+    timer.textContent = Number(timer.textContent) - 1;
+    if (Number(timer.textContent) == 0) {
+        alert(message);
+        clearInterval(timerset);
+        location = "https://ru.wikipedia.org/wiki/Lampropeltis_annulata";
+    }
+}, 1000)
