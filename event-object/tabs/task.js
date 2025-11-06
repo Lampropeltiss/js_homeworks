0d2f5e2f13ca5b65e28228a8dd487daeb7678f86
Nika <slesareva-n-r@ya.ru>
2025-11-06 15:54:10 +0300

    task: tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab__content");
+
+const toggleTabs = (num) => {
+    for (let i = 0; i < buttons.length; i++) {
+        if (i == num) {
+            buttons[i].classList.add("tab_active");
+            contents[i].classList.add("tab__content_active");
+        } else {
+            buttons[i].classList.remove("tab_active");
+            contents[i].classList.remove("tab__content_active");
+        }
+    }
+
+
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = () => {
+        toggleTabs(i);
+    }
+}
