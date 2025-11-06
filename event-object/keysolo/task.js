d657badbc1dad821cfb5b20be241d51032af7119
Nika <slesareva-n-r@ya.ru>
2025-11-06 14:41:10 +0300

    task: keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,10 +25,25 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+
+    document.addEventListener('keydown', e => {
+      console.log(e.key);
+      console.log(this.currentSymbol.textContent);
+      console.log(e.key == this.currentSymbol.textContent);
+
+      // if (e.key == this.currentSymbol.textContent) {
+      //   this.success();
+      // } else {
+      //   this.fail();
+      // }
+
+      this[e.key === this.currentSymbol.textContent ? 'success' : 'fail']();
+      
+    });
   }
 
   success() {
-    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    if (this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
@@ -60,18 +75,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -81,7 +96,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
