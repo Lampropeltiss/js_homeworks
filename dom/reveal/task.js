5d33f6dee6de9eaeb96dbc39223e1e1ad58dd032
Nika <slesareva-n-r@ya.ru>
2025-11-10 11:42:11 +0300

    task: reveal edited

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,11 +1,16 @@
-const block = document.querySelector(".reveal");
+const blocks = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
-    const {top, bottom} = block.getBoundingClientRect();
 
-    if (bottom > 0 && top < window.innerHeight) {
-        block.classList.add("reveal_active");
-    } else {
-        block.classList.remove("reveal_active");
-    }
+    blocks.forEach(
+        (block) => {
+            const { top, bottom } = block.getBoundingClientRect();
+
+            if (bottom > 0 && top < window.innerHeight) {
+                block.classList.add("reveal_active");
+            } else {
+                block.classList.remove("reveal_active");
+            }
+        }
+    )
 })
